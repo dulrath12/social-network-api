b46b84a69cac60e1279fe5bbc2fb47df803ed8b8
Dakotah Ulrath <dulrath@live.com>
2022-08-31 16:02:40 -0400

    server.js setup and complete, connection.js also setup and complete

---
+++ config/connection.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose')
+
+const connectionString = process.env.MONGODB_URI
+
+mongoose.connect(connectionString, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+
+
+
+module.exports = mongoose.connection
